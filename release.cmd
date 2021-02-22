bf6f586b3cb22cfc2289605ca0c18204804d4ceb
JustOff <Off.Just.Off@gmail.com>
2021-02-22 23:52:23 +0200

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.0.5
+set VER=1.0.6
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/youtube-lazy-load-.+?\.xpi/download\/%VER%\/youtube-lazy-load-%VER%\.xpi/" update.xml
